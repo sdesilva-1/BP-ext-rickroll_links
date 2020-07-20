bfd364d8770988d5b960a232d5e2b48b4503533e
sdesilva-1 <shanimanjula@gmail.com>
2020-07-20 17:31:20 -0400

    dADFERGVAg

---
+++ background.js
@@ -0,0 +1,19 @@
+
+var elements = document.querySelectorAll("a");
+for (var i= 0; i < elements.length; i++) {
+    elements[i].style.color = "green";
+    if(elements[i].tagName =="IMG"){
+      
+    }
+}
+
+/*
+var elements = document.querySelectorAll("a");
+for (var i= 0; i < elements.length; i++) {
+    elements[i].style.color = "green";
+    var x = elements[i];
+    if(x.tagName=="IMG"){
+        elements[i].src="replaced.jpg";
+    }
+}
+*/
