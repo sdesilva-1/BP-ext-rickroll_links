2f78c82959567ba586a6a59ce171ef01fd8b8937
sdesilva-1 <59806059+sdesilva-1@users.noreply.github.com>
2020-08-13 09:43:44 -0400

    Update content2b.js

--- content2b.js
+++ content2b.js
@@ -1,7 +1,7 @@
 chrome.storage.local.get(['rickroll'], function(items) {
   let rickroll = items["rickroll"];
   if (rickroll==1){
-    window.open("page.html");
+    window.open("page.html");//this bit doesn't work
     alert("yep");
     chrome.storage.local.set({"rickroll": 0});
   }
