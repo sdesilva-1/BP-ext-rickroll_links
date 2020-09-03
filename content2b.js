6c0a408340096caf2872c024a518a733f3db94b7
sdesilva-1 <shanimanjula@gmail.com>
2020-09-03 13:23:41 -0400

    trial solution for opening page.html
    https://stackoverflow.com/questions/35846565/open-a-local-html-on-extension-mouse-click
    also leo helped

--- content2b.js
+++ content2b.js
@@ -1,8 +1,7 @@
 chrome.storage.local.get(['rickroll'], function(items) {
   let rickroll = items["rickroll"];
   if (rickroll==1){
-    window.open("page.html");//this bit doesn't work
-    alert("yep");
+    chrome.tabs.create({ url: chrome.runtime.getURL("page.html") });
     chrome.storage.local.set({"rickroll": 0});
   }
 });
