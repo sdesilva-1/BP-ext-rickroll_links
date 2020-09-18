5fec4f15e70b0bb268856c850e6f7724e4796f76
sdesilva-1 <shanimanjula@gmail.com>
2020-09-18 11:03:24 -0400

    fdsfargrgaefdzcv

--- content2b.js
+++ content2b.js
@@ -1,7 +1,7 @@
 chrome.storage.local.get(['rickroll'], function(items) {
   let rickroll = items["rickroll"];
   if (rickroll==1){
-    chrome.tabs.create({ url: chrome.runtime.getURL("page.html") });
+    //OPEN PAGE.HTML HERE!!!
     chrome.storage.local.set({"rickroll": 0});
   }
 });
